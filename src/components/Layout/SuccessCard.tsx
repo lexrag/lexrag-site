d29f8c32ee20ed572060f0c60a60a1927bb97980
QuantumWolt <advkhazov@gmail.com>
2025-02-13 14:41:29 +0800

    static media is now on cdn.lexrag.com

--- src/components/Layout/SuccessCard.tsx
+++ src/components/Layout/SuccessCard.tsx
@@ -5,8 +5,8 @@ const SuccessCard = ({children}: PropsWithChildren) => {
     return (
         <Card>
             <div className="flex justify-center mb-5">
-                <img src="https://lexrag.com/custom_icons/light/__medal.svg" className="dark:hidden max-h-[180px]" alt="Action succeed!"/>
-                <img src="https://lexrag.com/custom_icons/dark/__medal.svg" className="light:hidden max-h-[180px]" alt="Action succeed!"/>
+                <img src="https://cdn.lexrag.com/custom_icons/light/__medal.svg" className="dark:hidden max-h-[180px]" alt="Action succeed!"/>
+                <img src="https://cdn.lexrag.com/custom_icons/dark/__medal.svg" className="light:hidden max-h-[180px]" alt="Action succeed!"/>
             </div>
 
             {children}
