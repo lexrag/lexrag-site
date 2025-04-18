8908fda4e4719f93ac6e3fc8baed2622931b77a1
alexcarv318 <alexilchenko2005@gmail.com>
2025-04-18 03:39:35 +0100

    Changed the docker file to parse env vars

--- src/utils/auth/setSession.ts
+++ src/utils/auth/setSession.ts
@@ -3,7 +3,7 @@
 import {cookies} from "next/headers";
 
 export const setSession = async (session: string) => {
-    const expiresAt = new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION_MINUTES) * 60 * 1000)
+    const expiresAt = new Date(Date.now() + parseInt(process.env.NEXT_PUBLIC_JWT_EXPIRATION_MINUTES) * 60 * 1000)
     const cookieStore = await cookies()
 
     cookieStore.set('session', session, {
