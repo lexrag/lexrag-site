4b6ad9ae9df54bb4dcf695e570bbe3a22f2034d8
QuantumWolt <advkhazov@gmail.com>
2025-06-11 18:37:50 +0800

    empty token & google auth fix

--- src/api/auth/getMeClient.ts
+++ src/api/auth/getMeClient.ts
@@ -1,15 +1,25 @@
 "use client"
 
 export const getMeClient = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/me`, {
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("token")}`,
-        }
-    });
+    const token = localStorage.getItem("token");
 
-    if (response.ok) {
-        return await response.json();
-    } else {
+    if (!token) {
         return null;
     }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/me`, {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            }
+        });
+
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (error) {
+        console.error("Error fetching /auth/me:", error);
+    }
+
+    return null;
 }
