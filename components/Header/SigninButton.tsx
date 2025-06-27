d38a37ef21658854fa7d5463aefbc58a0eacb48a
QuantumWolt <advkhazov@gmail.com>
2025-06-27 19:28:59 +0800

    minor changes on landing

--- components/Header/SigninButton.tsx
+++ components/Header/SigninButton.tsx
@@ -2,19 +2,18 @@ import Link from 'next/link';
 import { ShinyButton } from '@/components/magicui/shiny-button';
 
 const SigninButton = () => {
-    return (
-        <div className="flex items-center gap-8">
-            <div className="tab">
-                <Link href="/auth/signin" passHref>
-                    <ShinyButton className="bg-primary text-white px-5 py-3 border-none rounded-lg py-2 px-5">
-                        Sign In
-                    </ShinyButton>
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+           
+    <div className="flex items-center gap-8">
+      <div className="tab">
+        <Link href="/auth/signin" passHref>
+          <ShinyButton className="bg-primary text-white border-none rounded-lg py-2 px-4 transition-transform duration-300 hover:scale-105 shadow-lg">
+            Sign In
+          </ShinyButton>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SigninButton;
-
-// h-[2rem] whitespace-nowrap
--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -43,29 +43,50 @@ const LandingHeading = () => {
                     ></div>
                 </div>
 
-                <div className="relative text-center">
-                    <h1
-                        className="text-white tracking-wide font-semibold text-2xl md:text-4xl mb-[20px] transition-all duration-300"
-                        style={{
-                            lineHeight: '50px',
-                            textShadow: '1px 1px 4px rgba(0, 0, 0, 0.2)',
-                        }}
-                    >
-                        <AuroraText>Confidence in Every Decision</AuroraText>
-                        <br />
-                        legal AI with graph-vector context &nbsp;
-                    </h1>
+        <div
+          className="absolute inset-0 bg-no-repeat bg-contain bg-bottom opacity-40"
+          style={{
+            backgroundImage: 'url(/media/images/singapore_1.svg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center bottom',
+            backgroundBlendMode: 'multiply',
+          }}
+        >
+          <div
+            className="absolute inset-0 opacity-90"
+            style={{
+              background:
+                'linear-gradient(to bottom, rgba(19, 38, 60, 0.9) 0%, rgba(19, 38, 60, 0.95) 100%)',
+            }}
+          ></div>
+        </div>
 
-                    <Link href="/auth/signin" passHref>
-                        <ShinyButton className="bg-primary text-white px-5 py-3 border-none rounded-lg py-3 px-6">
-                            Try Lexrag
-                        </ShinyButton>
-                    </Link>
-                </div>
-            </div>
-            <CurveDown />
-        </>
-    );
+        <div className="relative text-center">
+          <h1
+            className="text-white tracking-wide font-semibold text-4xl md:text-4xl mb-[20px] transition-all duration-300"
+            style={{
+              lineHeight: '70px',
+              textShadow: '1px 1px 4px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            <AuroraText className='text-4xl md:text-5xl font-bold tracking-tight'>
+              Confidence in Every Decision
+            </AuroraText>
+            <br />
+            legal AI with graph-vector context &nbsp;
+          </h1>
+
+          <Link href="/chat" passHref>
+            <ShinyButton className="bg-primary text-white px-5 py-3 border-none rounded-lg py-2 px-4 transition-transform duration-300 hover:scale-110 shadow">
+              Try Lexrag
+            </ShinyButton>
+          </Link>
+        </div>
+      </div>
+      <CurveDown />
+    </>
+  );
 };
 
 export default LandingHeading;
