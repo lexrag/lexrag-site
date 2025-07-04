6e795e4dd47f3684e1cc0cc4b7e57f93c2cafa33
QuantumWolt <advkhazov@gmail.com>
2025-07-04 22:05:07 +0800

    conflict fix

--- components/UserProfile/PersonalInfoCard.tsx
+++ components/UserProfile/PersonalInfoCard.tsx
@@ -1,58 +1,39 @@
-import { RiEditBoxLine, RiImageAddLine } from '@remixicon/react';
 import { User } from '@/types/User';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { Card, CardContent, CardHeader, CardHeading, CardTitle } from '../ui/card';
+import CardWrapper from '../ui/card-wrapper';
+import Row from './Row';
 
 interface PersonalInfoCardProps {
     currentUser: User;
 }
 
 const PersonalInfoCard = ({ currentUser }: PersonalInfoCardProps) => {
+    const rows = [
+        {
+            label: 'Photo',
+            value: '150x150px JPEG, PNG Image',
+            actionIcon: (
+                <Avatar className="size-12 border-2 rounded-full border-green-500">
+                    <AvatarImage src="" />
+                    <AvatarFallback>V</AvatarFallback>
+                </Avatar>
+            ),
+        },
+        { label: 'First Name', value: currentUser.first_name },
+        { label: 'Last Name', value: currentUser.last_name },
+        { label: 'Birthday', value: '28 May 1996' },
+        { label: 'Gender', value: 'Male' },
+        { label: 'Address', value: 'Kyiv, Ukraine' },
+    ];
+
     return (
-        <div className="container-flex w-full pl-[15%] pr-[15%]">
-            <Card>
-                <CardHeader>
-                    <CardHeading>
-                        <CardTitle>Personal Info</CardTitle>
-                    </CardHeading>
-                </CardHeader>
-                <CardContent className="py-1">
-                    <div className="flex items-center justify-between gap-2 py-4 border-b border-dashed last:border-none">
-                        <div className="w-[20%] text-sm font-medium">Photo</div>
-                        <div className="flex-1 text-sm">150x150px JPEG, PNG Image</div>
-                        <div className="w-[10%] flex justify-center text-gray-500 hover:text-primary cursor-pointer">
-                            <Avatar className="size-12">
-                                <AvatarImage src={''} alt="Avatar" />
-                                <AvatarFallback>
-                                    <RiImageAddLine className="size-4" />
-                                </AvatarFallback>
-                            </Avatar>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-between gap-2 py-4 border-b border-dashed last:border-none">
-                        <div className="w-[20%] text-sm font-medium">First Name</div>
-                        <div className="flex-1 text-sm">{currentUser.first_name}</div>
-                        <div className="w-[10%] flex justify-center text-gray-500 hover:text-primary cursor-pointer">
-                            <RiEditBoxLine />
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-between gap-2 py-4 border-b border-dashed last:border-none">
-                        <div className="w-[20%] text-sm font-medium">Last Name</div>
-                        <div className="flex-1 text-sm">{currentUser.last_name}</div>
-                        <div className="w-[10%] flex justify-center text-gray-500 hover:text-primary cursor-pointer">
-                            <RiEditBoxLine />
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-between gap-2 py-4 border-b border-dashed last:border-none">
-                        <div className="w-[20%] text-sm font-medium">Email</div>
-                        <div className="flex-1 text-sm">{currentUser.email}</div>
-                        <div className="w-[10%] flex justify-center text-gray-500 hover:text-primary cursor-pointer">
-                            <RiEditBoxLine />
-                        </div>
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
+        <CardWrapper title="Personal Info">
+            {rows.map((item) => (
+                <Row key={item.label} label={item.label} actionIcon={item.actionIcon}>
+                    {item.value}
+                </Row>
+            ))}
+        </CardWrapper>
     );
 };
 
