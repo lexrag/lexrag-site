01e899acc6422ea1159a6ed69bb9c950a4702333
CosmoPush <maksymlopachak@gmail.com>
2025-08-12 13:02:31 +0300

    feat(ui): add checkmark icon hover effect

--- components/Landing/BenefitsCard.tsx
+++ components/Landing/BenefitsCard.tsx
@@ -1,3 +1,4 @@
+import CheckmarkIcon from '../icons/CheckMarkIcon';
 import { H4, PBase } from '../ui/typography';
 
 type BenefitsCardProps = {
@@ -5,12 +6,21 @@ type BenefitsCardProps = {
 };
 
 const BenefitsCard = ({ item }: BenefitsCardProps) => {
+    const getIcon = (iconName: string) => {
+        switch (iconName) {
+            case 'medal':
+                return <CheckmarkIcon />;
+            default:
+                return <img src={`/media/images/${iconName}.svg`} className="max-h-24 mb-4" alt={`${item.title}`} />;
+        }
+    };
+
     return (
         <div className="relative flex flex-col items-center justify-center text-center rounded-[30px] overflow-hidden h-full min-h-[300px] p-6">
             <div className="absolute inset-0 bg-[rgba(23,36,92,0.12)] backdrop-blur-md shadow-[0_0_25px_0_rgba(0,0,0,0.12)] z-0" />
 
             <div className="relative z-10 flex flex-col items-center h-full justify-center">
-                <img src={`/media/images/${item.img}.svg`} className="max-h-24 mb-4" alt={`${item.title}`} />
+                {getIcon(item.img)}
 
                 <div className="flex justify-center items-center mb-3">
                     <H4 className="text-white text-lg">{item.title}</H4>
