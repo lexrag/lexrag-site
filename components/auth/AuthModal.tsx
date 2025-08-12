335c27d5cf0ba31f0502b83d35ab4e963357cd87
CosmoPush <maksymlopachak@gmail.com>
2025-08-12 17:57:26 +0300

    chore(ui): refine aithomal styling and padding

--- components/auth/AuthModal.tsx
+++ components/auth/AuthModal.tsx
@@ -20,9 +20,12 @@ export default function AuthModal({ open, onOpenChange, initialMode = 'signin' }
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent className="sm:max-w-md w-full max-w-sm mx-auto rounded-[24px] bg-[linear-gradient(180deg,_rgba(255,255,255,0.30)_-1.72%,_rgba(255,255,255,0.60)_100%)]">
+            <DialogContent
+                className="p-16 sm:max-w-md w-full max-w-sm mx-auto rounded-[24px] backdrop-blur-[200px] 
+            border-[rgba(255,255,255,0.3)] bg-[rgba(178,173,173,0.30)]"
+            >
                 <DialogHeader>
-                    <DialogTitle className="text-2xl font-semibold text-left text-[var(--Brand-Primary-Midnight-Core)]">
+                    <DialogTitle className="text-3xl font-semibold text-left text-[var(--Brand-Primary-Midnight-Core)]">
                         Get Started Now
                     </DialogTitle>
                 </DialogHeader>
