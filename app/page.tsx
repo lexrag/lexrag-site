ca5273e61e2e745bc97f9010ee1563db8972479b
astraaaal-dev <vasylmyklashwrk@gmail.com>
2025-08-18 12:43:24 +0300

    comming soon feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { Metadata } from 'next';
-import { H3 } from '@/components/ui/typography';
-import ProductFeatures from '@/components/Features/FeaturesGrid';
-import Header from '@/components/Header/Header';
-import Benefits from '@/components/Landing/Benefits';
-import Footer from '@/components/Landing/Footer';
-import Hero from '@/components/Landing/Hero';
-import UseCases from '@/components/Landing/UseCases';
+// import { H3 } from '@/components/ui/typography';
+// import ProductFeatures from '@/components/Features/FeaturesGrid';
+// import Header from '@/components/Header/Header';
+// import Benefits from '@/components/Landing/Benefits';
+// import Footer from '@/components/Landing/Footer';
+// import Hero from '@/components/Landing/Hero';
+// import UseCases from '@/components/Landing/UseCases';
 import '@/css/themes/reui.css';
-import BackgroundSVG from '@/components/Landing/BackgroundSVG';
+import Link from 'next/link';
+import { Timer } from '@/components/ui/timer';
+import LiquidGlass from '@/components/liquid-glass';
+
+// import BackgroundSVG from '@/components/Landing/BackgroundSVG';
 
 export const metadata: Metadata = {
     title: 'LEXRAG',
@@ -15,25 +19,62 @@ export const metadata: Metadata = {
 
 const LandingPage = () => {
     return (
-        <div className="overflow-y-auto overflow-x-hidden bg-gradient-to-br from-[#D4D8E6] via-[#EBF0FF] to-[#D4D8E6] min-h-screen">
-            <Header className="" />
+        <div className="overflow-hidden bg-gradient-to-br from-[#D4D8E6] via-[#EBF0FF] to-[#D4D8E6] flex-grow-1">
+            {/* <Header className="" /> */}
 
-            <main className="px-[10%] z-1 relative">
-                <BackgroundSVG />
-                <section>
+            <main className="px-[10%] z-1 relative text-center">
+                <div className="max-w-[800px]  mx-auto mt-40 border border-white overflow-hidden rounded-[88px]">
+                    <LiquidGlass
+                        className="group transition-all duration-200 w-full h-full"
+                        centered={false}
+                        compact
+                        displacementScale={50}
+                        blurAmount={0.01}
+                        saturation={130}
+                        aberrationIntensity={2}
+                        elasticity={0.05}
+                        cornerRadius={80}
+                        mode="standard"
+                        padding="8px 16px"
+                        style={{
+                            boxShadow: 'none',
+                            filter: 'none',
+                        }}
+                    >
+                        <div className="p-8 md:py-16">
+                            <h1 className="text-2xl md:text-7xl font-extrabold text-white">Cooming soon</h1>
+                            <h5 className="pl-2 mt-1 text-sm md:text-2xl font-bold text-white">
+                                Our website is under construction
+                            </h5>
+
+                            <div className="flex justify-center mt-8">
+                                <Timer />
+                            </div>
+
+                            <Link
+                                href="#"
+                                className="text-sm md:text-xl mt-8 inline-flex items-center justify-center rounded-full px-6 py-3 font-extrabold text-white border border-white text-center hover:opacity-90"
+                            >
+                                Follow us for updates!
+                            </Link>
+                        </div>
+                    </LiquidGlass>
+                </div>
+
+                {/* <BackgroundSVG /> */}
+                {/* <section>
                     <Hero />
-                </section>
+                </section> */}
 
-                <section id="benefits" className="pl-6">
+                {/* <section id="benefits" className="pl-6">
                     <Benefits />
                 </section>
 
                 <section id="how-it-works" className="">
                     <UseCases />
-                </section>
+                </section> */}
 
-                <section id="product-features" className="">
-                    {/* TODO: keep sections title separately for reuse */}
+                {/* <section id="product-features" className="">
                     <div className="mb-10">
                         <H3 className="mb-2 transition-colors duration-300 text-[var(--color-midnight-core)] pl-8">
                             Product Features
@@ -46,10 +87,21 @@ const LandingPage = () => {
                         showBottomBadges={true}
                         maxHeightBeforeShowAll={650}
                     />
-                </section>
+                </section> */}
             </main>
 
-            <Footer />
+            <video
+                className="pointer-events-none select-none absolute inset-0 h-full w-full object-cover z-0 motion-reduce:hidden"
+                src="/media/video/3d_graph_demo.m4v"
+                autoPlay
+                muted
+                loop
+                playsInline
+                preload="metadata"
+                aria-hidden="true"
+            />
+
+            {/* <Footer /> */}
         </div>
     );
 };
